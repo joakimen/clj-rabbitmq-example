31a0c4e6e9f754a301a8a78421de414383105a9f
Joakim L. Engeset <joakim.engeset@gmail.com>
2021-09-05 14:10:41 +0200

    Cleanup project def

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject clj-queue-consumer-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "RabbitMQ-consumer in Clojure"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.novemberain/langohr "5.2.0"]]
   :main ^:skip-aot clj-queue-consumer-example.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :uberjar-name "app.jar"}})
